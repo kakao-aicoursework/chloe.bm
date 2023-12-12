b729eecb1d7e5b83b29fc765f05a1f999eacc69d
hbm0411 <hbm0411@gmail.com>
2023-12-12 18:08:06 +0900

    Project2: callback 메시지 발송 성공

--- langchain-project/api.py
+++ langchain-project/api.py
@@ -35,13 +35,13 @@ async def skill(req: ChatbotRequest):
 # callback.py 로 연결
 @app.post("/skill/callback")
 async def skill(req: ChatbotRequest, background_tasks: BackgroundTasks):
-    #핸들러 호출 / background_tasks 변경가능
-    background_tasks.add_task(callback_handler, req)
     out = {
-        "version" : "2.0",
-        "useCallback" : True,
+        "version": "2.0",
+        "useCallback": True,
         "data": {
-            "text" : "생각하고 있는 중이에요😘 \n15초 정도 소요될 거 같아요 기다려 주실래요?!"
+            "text": "생각하고 있는 중이에요😘 \n15초 정도 소요될 거 같아요 기다려 주실래요?!"
         }
     }
+    # 핸들러 호출 / background_tasks 변경가능
+    background_tasks.add_task(callback_handler, req)
     return out
--- langchain-project/callback.py
+++ langchain-project/callback.py
@@ -1,3 +1,5 @@
+import json
+
 from dto import ChatbotRequest
 from samples import list_card
 import aiohttp
@@ -6,27 +8,35 @@ import logging
 import openai
 from dotenv import load_dotenv
 import os
+import requests
 
 load_dotenv()
 openai.api_key = os.getenv('GPT_KEY', '')
 SYSTEM_MSG = "당신은 카카오 서비스 제공자입니다."
 logger = logging.getLogger("Callback")
 
-async def callback_handler(request: ChatbotRequest) -> dict:
+
+def callback_handler(request: ChatbotRequest) -> dict:
+    url = request.userRequest.callbackUrl
+    utterance = request.userRequest.utterance
+
+    print("url : ", url)
+    print("utterance : ", utterance)
 
     # ===================== start =================================
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": SYSTEM_MSG},
-            {"role": "user", "content": request.userRequest.utterance},
+            {"role": "user", "content": utterance},
         ],
         temperature=0,
     )
     # focus
     output_text = response.choices[0].message.content
+    print(output_text)
 
-   # 참고링크 통해 payload 구조 확인 가능
+    # 참고링크 통해 payload 구조 확인 가능
     payload = {
         "version": "2.0",
         "template": {
@@ -44,10 +54,12 @@ async def callback_handler(request: ChatbotRequest) -> dict:
     # 참고링크1 : https://kakaobusiness.gitbook.io/main/tool/chatbot/skill_guide/answer_json_format
 
     time.sleep(1.0)
-
-    url = request.userRequest.callbackUrl
+    print(payload)
 
     if url:
-        async with aiohttp.ClientSession() as session:
-            async with session.post(url=url, json=payload, ssl=False) as resp:
-                await resp.json()
+        response = requests.post(
+            url=url,
+            data=json.dumps(payload),
+            headers={"Content-Type": "application/json"}
+        )
+        print(response)
